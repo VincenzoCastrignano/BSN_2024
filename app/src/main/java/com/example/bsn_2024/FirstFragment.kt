bbe69090960f45553f59add75703411a05e13846
VincenzoCastrignano <91130104+VincenzoCastrignano@users.noreply.github.com>
2022-12-02 16:31:07 +0100

    Revert "Dev vincenzo"

--- app/src/main/java/com/example/bsn_2024/FirstFragment.kt
+++ app/src/main/java/com/example/bsn_2024/FirstFragment.kt
@@ -1,15 +1,10 @@
 package com.example.bsn_2024
 
 import android.os.Bundle
-import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.bsn_2024.databinding.FragmentFirstBinding
 
@@ -24,15 +19,6 @@ class FirstFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    private fun countMe(view: View) {
-        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
-        val countString = showCountTextView.text.toString()
-        var count = countString.toInt()
-        count++
-        showCountTextView.text = count.toString()
-    }
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -46,15 +32,8 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.randomButton.setOnClickListener {
+        binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-        view.findViewById<Button>(R.id.count_button).setOnClickListener {
-            countMe(view)
-        }
-        view.findViewById<Button>(R.id.Toast_button).setOnClickListener{
-            val myToast = Toast.makeText(context, "Bonjour je suis un toast", Toast.LENGTH_SHORT)
-            myToast.show()
         }
     }
 
@@ -62,5 +41,4 @@ class FirstFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
 }
