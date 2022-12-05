365e4e7ad5d78ace03a9e72722fe3a3ff0f69875
meyer.max <meyer.max411@gmail.com>
2022-12-05 16:52:23 +0100

    commit à la fin 05 décembre 2022

--- app/src/main/java/com/example/bsn_2024/FirstFragment.kt
+++ app/src/main/java/com/example/bsn_2024/FirstFragment.kt
@@ -1,10 +1,15 @@
 package com.example.bsn_2024
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.bsn_2024.databinding.FragmentFirstBinding
 
@@ -19,6 +24,7 @@ class FirstFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,8 +38,15 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.randomButton.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        view.findViewById<Button>(R.id.count_button).setOnClickListener {
+            Companion.countMe(view)
+        }
+        view.findViewById<Button>(R.id.Toast_button).setOnClickListener{
+            val myToast = Toast.makeText(context, "Bonjour je suis un toast", Toast.LENGTH_SHORT)
+            myToast.show()
         }
     }
 
@@ -41,4 +54,15 @@ class FirstFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    companion object {
+        private fun countMe(view: View) {
+            val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+            val countString = showCountTextView.text.toString()
+            var count = countString.toInt()
+            count++
+            showCountTextView.text = count.toString()
+        }
+    }
+
 }
