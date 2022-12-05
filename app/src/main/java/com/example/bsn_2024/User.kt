b54209106d89aae6c94967eb3801c7f52cf3b822
VincenzoCastrignano <vincenzo.castrignano@esqese.ucly.fr>
2022-12-05 16:48:59 +0100

    Ajout fin séance

--- app/src/main/java/com/example/bsn_2024/User.kt
+++ app/src/main/java/com/example/bsn_2024/User.kt
@@ -1,47 +1,61 @@
 package com.example.bsn_2024
 
-// TESTTT
-
 
 import java.util.Dictionary
 
-class User (
-    var ID: Int,
-    var fami_name: String,
-    var first_name: String,
-    var age: Int,
-    var mail: String,
-    var geoloc: Boolean,
-    var friend: Array<String>,
-    var conversation: Array<String>,
-    var config: Array<String>,
-    var user_name: String,
-    var user_pwd: String,
-    var phone: String,
-    var sex: Boolean,
-    var trajet: Array<String>,
-    var rdv: Array<String>
-){
+class User {
+    var ID: Int = 1
+    var last_name: String = "Castrignano"
+    var first_name: String ="Vincenzo"
+    var age: Int = 18
+    var mail: String = "vincenzo.castrignano@esqese.ucly.fr"
+    var geoloc: Boolean =true
+    var friend: Array<String> = arrayOf("")
+    var conversation: Array<String> = arrayOf("")
+    var config: Array<String> = arrayOf("")
+    var user_name: String = "Slayzen"
+    var user_pwd: String = "Slayzenmdp"
+    var phone: String = "0626273337"
+    var sex: Boolean = true
+    var trajet: Array<String> = arrayOf("")
+    var rdv: Array<String> = arrayOf("")
 
     init {
-        fami_name=fami_name
-        println("Le nom de la personne est $fami_name")
+        println("Le nom de la personne est $last_name")
     }
 
 
+    fun show_User() {
+
+    }
 
     fun Inscription() {
+        println("Methode Inscription User")
         /*
-        Toutes les données sont récupérer sur l'app
-        fami_name = Nom de famille
+        Toutes les données sont récupérées sur l'app
+        last_name = Nom de famille
         first_name = Prénom
         age = Age
-
          */
     }
 }
 
-
-fun main(){
-    val personne = User(1, "Castrignano", "Vincenzo", 19)
-}
+/*
+        (
+        val ID: Int,
+        val last_name: String,
+        val first_name: String,
+        val age: Int,
+        val mail: String,
+        val geoloc: Boolean,
+        val friend: Array<String>,
+        val conversation: Array<String>,
+        val config: Array<String>,
+        val user_name: String,
+        val user_pwd: String,
+        val phone: String,
+        val sex: Boolean,
+        val trajet: Array<String>,
+        val rdv: Array<String>
+        )
+*/
