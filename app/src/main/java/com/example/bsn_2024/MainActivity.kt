2e3bab7c8070399048f62f01f65ae008f7f0f3b5
Eliott <eliott.beaudouin@esqese.ucly.fr>
2022-12-12 16:27:45 +0100

    test

--- app/src/main/java/com/example/bsn_2024/MainActivity.kt
+++ app/src/main/java/com/example/bsn_2024/MainActivity.kt
@@ -22,11 +22,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
 
     }
