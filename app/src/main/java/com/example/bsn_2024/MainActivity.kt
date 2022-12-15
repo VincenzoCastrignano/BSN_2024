dbeee7464080aedb0f0e248c81b38a5dd0080e81
patigny.nicolas <Niiyugitowaifu1>
2022-12-15 09:35:13 +0100

    Ajouter Animation d'entrée

--- app/src/main/java/com/example/bsn_2024/MainActivity.kt
+++ app/src/main/java/com/example/bsn_2024/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import com.example.bsn_2024.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -23,12 +24,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
     }
 }
