9632c809cc0324bbd7fed85e8ebe5fd386f0efb2
patigny.nicolas <Niiyugitowaifu1>
2023-01-04 09:01:28 +0100

    Animation for main page

--- app/src/main/java/com/example/bsn_2024/MainActivity.kt
+++ app/src/main/java/com/example/bsn_2024/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bsn_2024
 
 import android.app.ActivityOptions
 import android.os.Bundle
+import android.text.TextUtils.replace
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -12,6 +13,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.Window
+import androidx.fragment.app.commit
 import com.example.bsn_2024.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -25,5 +27,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+
     }
+
+
 }
