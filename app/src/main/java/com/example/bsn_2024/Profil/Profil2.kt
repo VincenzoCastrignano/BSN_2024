6b5ef54c5e4601bc72b68cd43e89f81f56f20410
patigny.nicolas <Niiyugitowaifu1>
2023-01-26 15:47:30 +0100

    organisation

--- app/src/main/java/com/example/bsn_2024/Profil/Profil2.kt
+++ app/src/main/java/com/example/bsn_2024/Profil/Profil2.kt
@@ -1,10 +1,11 @@
-package com.example.bsn_2024
+package com.example.bsn_2024.Profil
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.bsn_2024.R
 import com.example.bsn_2024.databinding.Profil1Binding
 
 /**
