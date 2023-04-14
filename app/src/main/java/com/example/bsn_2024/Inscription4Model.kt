bbf24650aa915c9527d8eda748e0eed8ac64cb44
alhoussss <mohamedsori.alhousseini@esqese.ucly.fr>
2023-04-14 11:25:47 +0200

    continuer

---
+++ app/src/main/java/com/example/bsn_2024/Inscription4Model.kt
@@ -0,0 +1,7 @@
+package com.example.bsn_2024
+
+data class Inscription4Model (
+            var empId: String? = null,
+            var name: String? = null,
+            var lastname: String? = null,
+        )
